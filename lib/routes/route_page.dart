fe7e5def109b5d24784c61eff39fd5724cf36edc
matias <mdsmatias@hotmail.com>
2020-06-12 18:40:53 +0200

    Añado páginas setting y rooms al route-pages

--- lib/routes/route_page.dart
+++ lib/routes/route_page.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutterappdomotica/Rooms/ui/pages/rooms_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/home_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/policy_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/profile_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/register_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/init_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/login_page.dart';
+import 'package:flutterappdomotica/Users/ui/pages/setting_page.dart';
 import 'package:flutterappdomotica/Users/ui/pages/terms_page.dart';
 import 'package:flutterappdomotica/Widget/curved_navigation_bar_init.dart';
 import 'package:flutterappdomotica/constants.dart';
@@ -17,7 +19,9 @@ Map<String, WidgetBuilder> getApplicationRoutes() {
     policyPage    : (BuildContext context) => PolicyPage(),
     termsPage     : (BuildContext context) => TermsPage(),
     homePage      : (BuildContext context) => CurvedNavigationBarInit(),
-    profilePage      : (BuildContext context) => ProfilePage(),
+    profilePage   : (BuildContext context) => ProfilePage(),
+    roomsPage     : (BuildContext context) => RoomsPage(),
+    settingPage   : (BuildContext context) => SettingPage(),
 
 //    'forgotPasswordScreen': (BuildContext context) => ForgotPassword(),
 //    'homePage': (BuildContext context) => CurvedNavigationBarInit(),
