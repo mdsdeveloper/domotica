341598e17b4f2f4e76bb02625387b7f050e42b4b
matias <mdsmatias@hotmail.com>
2020-06-13 16:45:20 +0200

    Añado metodo que devuelve todas las habitaciones a bloc rooms

--- lib/Rooms/bloc/rooms_bloc.dart
+++ lib/Rooms/bloc/rooms_bloc.dart
@@ -1,5 +1,13 @@
 
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutterappdomotica/Users/api/cloud_firestore_api.dart';
+import 'package:flutterappdomotica/Users/repository/cloud_firestore_repository.dart';
+
 class RoomsBloc{
-  
+  final _cloudFirestoreRepository = CloudFirestoreRepository();
+
+  Stream<QuerySnapshot> allRoomsListSream() => _cloudFirestoreRepository.getAllRooms();
+
+
 }
