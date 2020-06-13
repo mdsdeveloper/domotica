5c165911ef9ed89e74c84a68cc6569526fe61f34
matias <mdsmatias@hotmail.com>
2020-06-13 16:47:28 +0200

    Añado metodo para recuperar todas la habitaciones de firestore

--- lib/Users/api/cloud_firestore_api.dart
+++ lib/Users/api/cloud_firestore_api.dart
@@ -4,25 +4,28 @@ import 'package:flutter/material.dart';
 import 'package:flutterappdomotica/Users/api/auth_api.dart';
 import 'package:flutterappdomotica/Users/model/user_model.dart';
 
-
 class CloudFirestoreAPI {
-
   final String USERS = "users";
+  final String ROOMS = "rooms";
 
   final Firestore _db = Firestore.instance;
   static FirebaseAuth _auth = FirebaseAuth.instance;
-
 
   void updateUserData(UserModel userModel) async {
     DocumentReference ref = _db.collection(USERS).document(userModel.uid);
     return await ref.setData({
       'uid': userModel.uid,
       'name': userModel.name,
-      'apellidos' : userModel.apellidos,
-      'nickName' : userModel.nickName,
+      'apellidos': userModel.apellidos,
+      'nickName': userModel.nickName,
       'email': userModel.email,
       'photoUrl': userModel.photoUrl,
       'lastSignIn': DateTime.now()
     }, merge: true);
+  }
+
+  Stream<QuerySnapshot> getAllRooms() {
+    var snapshots = _db.collection(CloudFirestoreAPI().ROOMS).snapshots();
+    return snapshots;
   }
 }
